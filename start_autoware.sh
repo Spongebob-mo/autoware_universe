4e0cb8906cd0b991cab9109e9c639606683e37da
Spongebob-mo <467480129@qq.com>
2024-09-10 17:27:55 +0800

    first commit

---
+++ start_autoware.sh
@@ -0,0 +1,2 @@
+source /autoware_universe/install/setup.bash
+ros2 launch autoware_launch planning_simulator.launch.xml map_path:=$HOME/autoware_map/sample-map-planning vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
